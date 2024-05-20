3f1b28763716eb56118d325a710e09c6a9e4cda3
ppdesai434 <parthprakash.desai@mavs.uta.edu>
2024-05-20 04:14:39 +0000

    Fixed flash attention and added deepspeed install commands

--- use-cases/multi-modal-llava-ft/02-install-llava-prereqs.sh
+++ use-cases/multi-modal-llava-ft/02-install-llava-prereqs.sh
@@ -1,8 +1,11 @@
 git clone https://github.com/haotian-liu/LLaVA.git 
 cd /home/sagemaker-user/LLaVA/ && pip install -e ".[train]"
 cd /home/sagemaker-user/
-pip install flash-attn==2.5.8 --no-build-isolation
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+pip install flash-attn==2.5.8 --no-build-isolation --no-cache-dir
 pip install tensorboardX==2.6.2.2
+pip install deepspeed
 wget http://images.cocodataset.org/zips/train2017.zip 
 unzip -Z1 train2017.zip | head -10000 | sed 's| |\\ |g' | xargs unzip train2017.zip
 echo "Sampling dataset..."
