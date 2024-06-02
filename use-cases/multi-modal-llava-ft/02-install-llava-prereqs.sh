24a222f8adc10d80eaf1067e0800694ee140e874
Parth Desai <parthdes@amazon.com>
2024-06-02 04:36:03 +0000

    Path change

--- use-cases/multi-modal-llava-ft/02-install-llava-prereqs.sh
+++ use-cases/multi-modal-llava-ft/02-install-llava-prereqs.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/haotian-liu/LLaVA.git 
-cd /home/sagemaker-user/LLaVA/ && pip install -e ".[train]"
-cd /home/sagemaker-user/
+cd ./LLaVA/ && pip install -e ".[train]"
+cd ..
 export PATH=/usr/local/cuda/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 pip install flash-attn==2.5.8 --no-build-isolation --no-cache-dir
@@ -10,4 +10,4 @@ pip install deepspeed
 wget http://images.cocodataset.org/zips/train2017.zip 
 unzip -Z1 train2017.zip | head -10000 | sed 's| |\\ |g' | xargs unzip train2017.zip
 echo "Sampling dataset..."
-python3 /home/sagemaker-user/download_and_sample_data.py
+python3 ./download_and_sample_data.py
--- use-cases/multi-modal-llava-ft/03-fine-tune-llava.sh
+++ use-cases/multi-modal-llava-ft/03-fine-tune-llava.sh
@@ -1,10 +1,10 @@
-deepspeed /home/sagemaker-user/LLaVA/llava/train/train_mem.py \
+deepspeed ./LLaVA/llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed /home/sagemaker-user/LLaVA/scripts/zero3.json \
+    --deepspeed ./LLaVA/scripts/zero3.json \
     --model_name_or_path liuhaotian/llava-v1.5-7b \
     --version v1 \
-    --data_path /home/sagemaker-user/sample-training-data.json \
-    --image_folder /home/sagemaker-user/train2017 \
+    --data_path ./sample-training-data.json \
+    --image_folder ./train2017 \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
@@ -13,7 +13,7 @@ deepspeed /home/sagemaker-user/LLaVA/llava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 False \
-    --output_dir ./checkpoints/llava-v1.5-13b-task-lora \
+    --output_dir ./llava-training-output-lora \
     --num_train_epochs 5 \
     --per_device_train_batch_size 5 \
     --per_device_eval_batch_size 2 \
